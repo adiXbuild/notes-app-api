a659805a2e2b3c5f2a481b8223af6ced4b35edff
Aditya_Shukla <masteradityashukla@gmail.com>
2026-08-08 01:37:21 +0530

    added .env file to gitignore

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");  // mongoose is a package, like express
+import dotenv from "dotenv";
+dotenv.config();
 
 async function connectdb(){
-    await mongoose.connect("mongodb+srv://learn-backend:DoGsx4MEmHLpHN6I@cluster0.nz37bax.mongodb.net/halley/"); // here under the round braces we need to add the connection string copied from the compass...
+    await mongoose.connect("process.env.Mongoose_url"); // here under the round braces we need to add the connection string copied from the compass...
     console.log("connected to database");
 }
 
